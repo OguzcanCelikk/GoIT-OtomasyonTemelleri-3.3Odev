2c618e647b468193062dec28ba37931d992db77b
Oğuzcan Çelik <oguzcancelik@192.168.1.12>
2026-01-27 18:41:12 +0300

    odev

---
+++ cypress/e2e/LoginTest.cy.js
@@ -0,0 +1,34 @@
+//////// Sayfaları çağırma
+import { loginPage } from '../pages/Login';
+import { homePage } from '../pages/HomePage';
+
+describe('GoIT Login ve Logout Testleri (POP Yapısı)', () => {
+
+  beforeEach(() => {
+    cy.viewport(1280, 720);
+    loginPage.navigate();
+  });
+
+  it('Test No 1: user888 ile giriş ve çıkış', () => {
+    /////// Sayfa butonlarını kullanarak giriş yap
+    loginPage.login('user888@gmail.com', '1234567890');
+
+    ////// Sayfa butonlarını kullanarak çıkış yap
+    homePage.logout();
+    
+    ////// Doğrulama
+    cy.url().should('include', '/account/login');
+  });
+
+  it('Test No 2: testowyqa ile giriş ve çıkış', () => {
+    //////// İkinci kullanıcı için giriş yap
+    loginPage.login('testowyqa@qa.team', 'QA!automation-1');
+
+    ///////// Çıkış yap
+    homePage.logout();
+    
+    ///////// Doğrulama
+    cy.url().should('include', '/account/login');
+  });
+
+});
